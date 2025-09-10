c20d6601d804d31cb435b62dbc4c3272794689d0
1Luis-Amaro <luis.tavio03@gmail.com>
2025-09-09 22:25:53 -0300

    117 - Componente FeaturedPost + Tailwind CSS Grid

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -2,12 +2,31 @@ import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group" >
+        <Link href="#" className="w-full h-full overflow-hidden rounded-xl">
+      <Image
+      className="group-hover:scale-105 transition"
+      src="/images/bryen_0.png"
+      width={1200}
+      height={768}
+      alt="Título do post"
+      />
+      </Link>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          vero consectetur quasi officia nesciunt, accusantium animi ut,
+          molestiae qui maxime quam porro minima voluptatem cum voluptatibus.
+          Ducimus ea rem dolorem.
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
