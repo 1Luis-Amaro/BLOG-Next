ae44cd4c956721c62c6b4b5536f95a537fe11d9d
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-09-29 13:10:01 -0300

    122 - Movendo PostFeatured para um componente separado

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
-import { PostCoverImage } from "@/components/PostCoverImage";
 import { PostsList } from "@/components/PostsList";
+import { PostFeatured } from "@/components/PostFeatured";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
@@ -9,21 +9,8 @@ export default async function HomePage() {
   return (
     <Container>
       <Header />
-      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-         <PostCoverImage
-          linkProps={{
-            href: '/post/asdfasdf',
-          }}
-          imageProps={{
-            width: 1200,
-            height: 720,
-            src: '/images/bryen_9.png',
-            alt: 'Alt da imagem',
-            priority: true,
-          }}
-        />
 
-      </section>
+      <PostFeatured />
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
