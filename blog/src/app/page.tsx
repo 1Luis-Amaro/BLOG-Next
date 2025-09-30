a5d02a919573ce204be2937cdffcbc64a574c28b
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-09-30 16:36:31 -0300

    127. Sobre layout.tsx (root) e Metadata do Next para title e description 12m

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,24 +1,13 @@
-import { Container } from "@/components/Container";
-import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
-import { PostFeatured } from "@/components/PostFeatured";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
-      <PostFeatured />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer>
-        <p className="text-6xl font-bold text-center py-8">Footer</p>
-      </footer>
-    </Container>
+    </>
   );
 }
