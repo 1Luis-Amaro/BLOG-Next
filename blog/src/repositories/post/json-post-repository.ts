e7af5a377881040b96b016d71483fda443deff50
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-09-30 11:26:05 -0300

    126 - React cache remove duplicação de chamadas da API ou Repositório

--- blog/src/repositories/post/json-post-repository.ts
+++ blog/src/repositories/post/json-post-repository.ts
@@ -16,13 +16,13 @@ const SIMULATE_WAIT_IN_MS = 0;
 
 
 export class JsonPostRepository implements PostRepository { // Define uma classe que implementa a interface PostRepository
-  
+
     private async simulateWait() {
     if (SIMULATE_WAIT_IN_MS <= 0) return;
 
     await new Promise(resolve => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
   }
-  
+
   private async readFromDisk(): Promise<PostModel[]> { // Método privado para ler o arquivo JSON do disco
       const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8'); // Lê o conteúdo do arquivo como texto
       const parsedJson = JSON.parse(jsonContent); // Converte o texto JSON em um objeto JavaScript
@@ -30,15 +30,15 @@ export class JsonPostRepository implements PostRepository { // Define uma classe
       return posts; // Retorna apenas o array de posts
     }
 
-     async findAll(): Promise<PostModel[]> { // Método público para buscar todos os posts
+     async findAllPublic(): Promise<PostModel[]> { // Método público para buscar todos os posts
       const posts = await this.readFromDisk(); // Chama o método privado para ler os posts do disco
-      return posts; // Retorna a lista de posts
+      return posts.filter(post => post.published); // Retorna a lista de posts
       }
 
       async findById(id: string): Promise<PostModel> { // Método público para buscar um post por ID
             await this.simulateWait();
 
-        const posts = await this.findAll(); // Busca todos os posts primeiro
+        const posts = await this.findAllPublic(); // Busca todos os posts primeiro
         const post = posts.find(post => post.id === id); // Procura o post com o ID especificado
 
         if(!post) throw new Error('Post não encontrado'); // Se não encontrar, lança um erro
