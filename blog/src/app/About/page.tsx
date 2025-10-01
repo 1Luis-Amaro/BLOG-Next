b930563cf501601abc657fecbded9e8ac8d642c0
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-10-01 16:29:17 -0300

    128 - Separando o componente Footer (rodapé)

--- blog/src/app/About/page.tsx
+++ blog/src/app/About/page.tsx
@@ -1,9 +1,3 @@
-import { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: 'About'
-}
-
-export default function AbouPage() {
+export default function AboutPage() {
   return <h1>Olá mundo</h1>
 }
--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,9 +38,7 @@ export default function RootLayout({
 
           {children}
 
-          <footer>
-            <p className="text-6xl font-bold text-center py-8">Footer</p>
-          </footer>
+    <Footer/>
         </Container>
       </body>
     </html>
