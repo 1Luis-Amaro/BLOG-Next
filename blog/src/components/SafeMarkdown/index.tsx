f8163fe3798c2a4986eb24cd4c40b52110eb63b4
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2025-10-06 15:53:23 -0300

    138 - Substituindo e modificando elementos HTML no React Markdown

--- blog/src/components/SafeMarkdown/index.tsx
+++ blog/src/components/SafeMarkdown/index.tsx
@@ -20,12 +20,22 @@ export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
         "prose-a:text-blue-500",
         "prose-a:hover:text-blue-700",
         'prose-img:mx-auto',
-        'md:prose-lg'
+        'lg:prose-lg'
       )}
     >
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({node, ...props}) => {
+            if (!node?.children) return ''
+            return (
+              <div className="overflow-x-auto">
+                  <table className="w-full min-w-[600px]"  {...props} />
+              </div>
+            )
+          }
+        }}
       >
         {markdown}
       </ReactMarkdown>
