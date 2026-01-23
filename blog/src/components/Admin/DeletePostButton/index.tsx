5d4081eee6ad62d40742b38e8f371b98f589c50f
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2026-01-23 16:55:14 -0300

    170. Dialog: adicionando as funcionalidades do componente

--- blog/src/components/Admin/DeletePostButton/index.tsx
+++ blog/src/components/Admin/DeletePostButton/index.tsx
@@ -1,39 +1,57 @@
 "use client";
 
 import { deletePostAction } from "@/actions/post/delete-post-action";
+import { Dialog } from "@/components/Dialog";
 import clsx from "clsx";
 import { Trash2Icon } from "lucide-react";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
 
-type DelePostButtonProps = {
+type DeletePostButtonProps = {
   id: string;
   title: string;
 };
 
-export function DeletePostButton({ id, title }: DelePostButtonProps) {
+export function DeletePostButton({ id, title }: DeletePostButtonProps) {
   const [isPending, startTransition] = useTransition();
+  const [showDialog, setShowDialog] = useState(false);
 
   function handleClick() {
-    if (!confirm("Tem certeza?")) return;
+    setShowDialog(true);
+
+  }
+  function handleConfirm() {
     startTransition(async () => {
       const result = await deletePostAction(id);
       alert(`O result é: ${result}`);
+      setShowDialog(false)
     });
   }
   return (
-    <button
-      className={clsx(
-        "text-red-500 cursor-pointer transition",
-        "[&_svg]:w-4 [&_svg]:h-4",
-        "hover:scale-125 hover:text-red-700",
-        "disabled:text-slate-600 disabled:cursor-not-allowed"
+    <>
+      <button
+        className={clsx(
+          "text-red-500 cursor-pointer transition",
+          "[&_svg]:w-4 [&_svg]:h-4",
+          "hover:scale-125 hover:text-red-700",
+          "disabled:text-slate-600 disabled:cursor-not-allowed"
+        )}
+        aria-label={`Apagar post: ${id}`}
+        title={`Apagar post: ${title}`}
+        onClick={handleClick}
+        disabled={isPending}
+      >
+        <Trash2Icon />
+      </button>
+      {showDialog && (
+        <Dialog
+          isVisible={showDialog}
+          title="Apagar post?"
+          content={`Tem certeza que deseja apagar o post: ${title}`}
+          onCancel={() => setShowDialog(false)}
+          onConfirm={handleConfirm}
+          disabled={isPending}
+        />
       )}
-      aria-label={`Apagar post: ${id}`}
-      title={`Apagar post: ${title}`}
-      onClick={handleClick}
-      disabled={isPending}
-    >
-      <Trash2Icon />
-    </button>
+    </>
   );
 }
--- blog/src/components/PostListAdmin/index.tsx
+++ blog/src/components/PostListAdmin/index.tsx
@@ -32,49 +32,6 @@ export default async function PostsListAdmin() {
         );
       })}
 
-      <div
-        className={clsx(
-          "fixed z-50 top-0 bottom-0 left-0 right-0 bg-black/50 backdrop-blur-xs",
-          "flex items-center justify-center"
-        )}
-      >
-        <div
-          className={clsx(
-            "bg-slate-100 p-6 rounded-lg max-w-2xl mx-6",
-            "flex flex-col gap-6",
-            "shadow-lg shadow-black/30 text-center"
-          )}
-        >
-          <h3 className="text-xl font-extrabold"  >Título do diálogo</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
-            dolores repellendus illum laboriosam? Debitis vero culpa sequi hic
-            amet impedit quasi voluptates laudantium pariatur quaerat! Ipsa
-            sapiente accusamus nam tempora?
-          </p>
-          <div className="flex items-center justify-around">
-            <button
-              className={clsx(
-                "bg-slate-200 hover:bg-slate-300 transition text-slate-950 ",
-                "flex items-center justify-center",
-                "py-2 px-4 rounded-lg cursor-pointer"
-              )}
-              autoFocus
-            >
-              Cancelar
-            </button>
-            <button
-              className={clsx(
-                "bg-blue-300 hover:bg-blue-600 transition text-blue-50 ",
-                "flex items-center justify-center",
-                "py-2 px-4 rounded-lg cursor-pointer"
-              )}
-            >
-              Ok
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
