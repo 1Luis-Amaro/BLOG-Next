3dc76cb1f4f37a2b0497e828470cc788256353be
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2026-02-20 11:53:53 -0300

    186 - Validando o arquivo de imagem enviado ao servidor

--- blog/src/components/Admin/ImageUploader/index.tsx
+++ blog/src/components/Admin/ImageUploader/index.tsx
@@ -1,43 +1,59 @@
 "use client";
 
+import { uploadImageAction } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/Button";
 import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/post/constants";
 import { ImageUpIcon } from "lucide-react";
-import { useRef } from "react";
+import { useRef, useTransition } from "react";
 import { toast } from "react-toastify";
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isUploading, startTransition] = useTransition();
 
-  function handleChosseFile(){
-    if(!fileInputRef.current) return
+  function handleChosseFile() {
+    if (!fileInputRef.current) return;
 
-    fileInputRef.current.click()
+    fileInputRef.current.click();
   }
 
   function handleChange() {
-        if(!fileInputRef.current) return
 
-      const fileInput = fileInputRef.current
-      const file = fileInput?.files?.[0]
+    toast.dismiss()
 
-      if (!file) return
+    if (!fileInputRef.current) return;
 
-      if(file.size > IMAGE_UPLOAD_MAX_SIZE ) {
-        const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024
-        toast.error(`Imagem muito grande. Máx.:${readableMaxSize}KB.`)
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
 
-        fileInput.value = ''
-        return
-      }
+    if (!file) return;
 
-      //TODO: criar a action para upload de arquivo
-      const formData = new FormData()
-      formData.append('file', file)
+    if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+      toast.error(`Imagem muito grande. Máx.:${readableMaxSize}KB.`);
 
-      fileInput.value = ''
+      fileInput.value = "";
+      return;
     }
 
+    //TODO: criar a action para upload de arquivo
+    const formData = new FormData();
+    formData.append("file", file);
+
+    startTransition(async () => {
+      const result = await uploadImageAction(formData);
+
+      if (result.error) {
+        toast.error(result.error);
+        fileInput.value = "";
+        return;
+      }
+
+      toast.success(result.url);
+    });
+
+    fileInput.value = "";
+  }
 
   return (
     <div className="flex flex-col gap-2 py-4">
@@ -45,7 +61,14 @@ export function ImageUploader() {
         <ImageUpIcon />
         Enviar uma imagem
       </Button>
-      <input onChange={handleChange} className="hidden" name="file" type="file" accept="image/*" ref={fileInputRef} />
+      <input
+        onChange={handleChange}
+        className="hidden"
+        name="file"
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+      />
     </div>
   );
 }
