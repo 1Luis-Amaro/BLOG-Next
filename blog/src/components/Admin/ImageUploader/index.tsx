4955041b44a7d40843427b4a9682ad0ef7ab44c9
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2026-02-20 17:54:15 -0300

    188 - Exibindo um preview e a URL da imagem após enviar para o servidor

--- blog/src/components/Admin/ImageUploader/index.tsx
+++ blog/src/components/Admin/ImageUploader/index.tsx
@@ -4,12 +4,13 @@ import { uploadImageAction } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/Button";
 import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/post/constants";
 import { ImageUpIcon } from "lucide-react";
-import { useRef, useTransition } from "react";
+import { useRef, useState, useTransition } from "react";
 import { toast } from "react-toastify";
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [isUploading, startTransition] = useTransition();
+  const [imgUrl, setImgUrl] = useState("");
 
   function handleChosseFile() {
     if (!fileInputRef.current) return;
@@ -18,21 +19,27 @@ export function ImageUploader() {
   }
 
   function handleChange() {
+    toast.dismiss();
 
-    toast.dismiss()
-
-    if (!fileInputRef.current) return;
-
+    if (!fileInputRef.current) {
+      setImgUrl("");
+      return;
+    }
     const fileInput = fileInputRef.current;
     const file = fileInput?.files?.[0];
 
-    if (!file) return;
+    if (!file) {
+      setImgUrl("");
+      return;
+    }
 
     if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
       const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
       toast.error(`Imagem muito grande. Máx.:${readableMaxSize}KB.`);
 
       fileInput.value = "";
+
+      setImgUrl("");
       return;
     }
 
@@ -46,21 +53,34 @@ export function ImageUploader() {
       if (result.error) {
         toast.error(result.error);
         fileInput.value = "";
+        setImgUrl("");
         return;
       }
 
-      toast.success(result.url);
+      setImgUrl(result.url);
+      toast.success("Imagem enviada");
     });
 
     fileInput.value = "";
   }
 
   return (
-    <div className="flex flex-col gap-2 py-4">
-      <Button onClick={handleChosseFile} type="button" className="self-start">
+    <div className="flex flex-col gap-4 py-4">
+      <Button onClick={handleChosseFile} type="button" className="self-start" disabled={isUploading}>
         <ImageUpIcon />
         Enviar uma imagem
       </Button>
+
+      {!!imgUrl && (
+        <div className="flex flex-col gap-4">
+          <p>
+            <b>URL:  </b>{imgUrl}
+          </p>
+          {/* eslint-disable-next-line */}
+          <img className="rounded-lg" src={imgUrl}  />
+        </div>
+      )}
+
       <input
         onChange={handleChange}
         className="hidden"
@@ -68,6 +88,7 @@ export function ImageUploader() {
         type="file"
         accept="image/*"
         ref={fileInputRef}
+        disabled={isUploading}
       />
     </div>
   );
