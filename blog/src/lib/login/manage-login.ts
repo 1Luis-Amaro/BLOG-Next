18d4bddc4c8f5fe099b7edc5698f56466ae4dbf3
Luis Otavio Miranda Amaro <luis.tavio03@gmail.com>
2026-06-19 17:06:58 -0300

    208 - Entendendo e assinando um JWT com a biblioteca jose

--- blog/src/lib/login/manage-login.ts
+++ blog/src/lib/login/manage-login.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs"
 import { cookies } from "next/headers"
+import { SignJWT, jwtVerify } from 'jose'
 
 const jwtSecretKey = process.env.JWT_SECRET_KEY
 const jwtEncodedKey = new TextEncoder().encode(jwtSecretKey)
@@ -8,6 +9,10 @@ const loginExpSeconds = Number(process.env.LOGIN_EXPIRATION_SECONDS) || 86400
 const loginExpStr = process.env.LOGIN_EXPIRATION_STRING || '1d'
 const loginCookieName = process.env.LOGIN_COOKIE_NAME || 'loginSession'
 
+type JwtPayLoad = {
+  username: string,
+  expiresAt: Date
+}
 
 export async function hashPassword(password: string) {
   const hash = await bcrypt.hash(password, 10)
@@ -17,12 +22,12 @@ export async function hashPassword(password: string) {
 export async function verifyPassword(password: string, base64Hash: string) {
   const hash = Buffer.from(base64Hash, 'base64')
     .toString('utf-8');
-    return bcrypt.compare(password, hash)
+  return bcrypt.compare(password, hash)
 }
 
 export async function createLoginSession(username: string) {
   const expiresAt = new Date(Date.now() + loginExpSeconds * 1000)
-  const loginSession = username + "QUALQUER COISA"
+  const loginSession = await signJwt({username, expiresAt})
   const cookieStore = await cookies()
 
   cookieStore.set(loginCookieName, loginSession, {
@@ -33,9 +38,19 @@ export async function createLoginSession(username: string) {
   })
 
 }
-  export async function deleteLoginSession() {
-    const cookieStore = await cookies()
-      cookieStore.set(loginCookieName, '', {expires: new Date(0)})
+export async function deleteLoginSession() {
+  const cookieStore = await cookies()
+  cookieStore.set(loginCookieName, '', { expires: new Date(0) })
+  cookieStore.delete(loginCookieName)
+}
 
-      cookieStore.delete(loginCookieName)
+export async function signJwt (jwtPayLoad: JwtPayLoad) {
+  return new SignJWT(jwtPayLoad)
+    .setProtectedHeader({
+      alg: 'HS256',
+      typ: 'JWT'
+    })
+    .setIssuedAt()
+    .setExpirationTime(loginExpStr)
+    .sign(jwtEncodedKey)
 }
